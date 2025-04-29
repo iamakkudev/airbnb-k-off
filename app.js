1bc84e32b527c708ac67689db4323942efc657f7
iamakkudev <akashsahani9795@gmail.com>
2025-04-29 08:05:30 +0530

    added client side validation and error handling

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const ejs = require('ejs');
 const methodOverride = require('method-override');
 const Listing = require('./models/listing.js');
 const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync.js');
+const ExpressError = require('./utils/ExpressError.js');
 
 app.set('view engine', 'ejs');
 app.set('views', Path.join(__dirname, 'views'))
@@ -29,40 +31,53 @@ app.get('/', (req, res) => {
     res.send('hii this is a root');
     });
 //all listings
-app.get('/listings', async(req, res) => {
+app.get('/listings',wrapAsync( async(req, res) => {
     const listing = await Listing.find({})
     res.render('./listings/index.ejs', { listing});
-})
+}))
 
 
 app.get('/listings/new', (req, res) => {
     res.render('./listings/new.ejs')
 })
 
-app.post('/listings', async(req, res) => {
+app.post('/listings',wrapAsync( async(req, res) => {
+    if(!req.body) throw new ExpressError(400, 'Send Valid Data');
     const listing = new Listing(req.body);
     await listing.save();
     res.redirect('/listings')
-})
-app.get('/listings/:id/edit', async(req, res) => {
+}))
+app.get('/listings/:id/edit',wrapAsync( async(req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render('./listings/edit.ejs', { listing});
-})
-app.put('/listings/:id', async(req, res) => {
+}))
+app.put('/listings/:id',wrapAsync( async(req, res) => {
+    if(!req.body) throw new ExpressError(400, 'Send Valid Data');
     let { id } = req.params;
     const listing = await Listing.findByIdAndUpdate(id, {...req.body}, { new: true });   
     res.redirect(`/listings/${id}`)   
-})
-app.delete('/listings/:id', async(req, res) => {
+}))
+app.delete('/listings/:id',wrapAsync( async(req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect('/listings')
-})
-app.get('/listings/:id', async(req, res) => {
+}))
+app.get('/listings/:id',wrapAsync( async(req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render('./listings/show.ejs', { listing});
+}))
+
+
+app.all('/*path', (req, res,next) => {
+    next(new ExpressError( 404,'Page not found'));
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = 'Something went wrong' } = err;
+    console.log(err.message);
+    res.status(statusCode).send(message);
 })
 
 
